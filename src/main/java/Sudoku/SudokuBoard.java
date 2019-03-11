c3912d1f4042e4d679691453ea0db8a2ab0b1e50
Maruduer98 <lukko98@gmail.com>
2019-03-11 19:47:06 +0100

    Dodano sprawdzanie i printowanie (chyba dziala ok)

--- src/main/java/Sudoku/SudokuBoard.java
+++ src/main/java/Sudoku/SudokuBoard.java
@@ -8,27 +8,45 @@ public class SudokuBoard {
 
     void fillBoard(){
         Random rand = new Random();
+        int x;
         for(int i=0; i<9; i++){
             for (int j=0; j<9;j++){
-                if(board[i][j]==0 && fits(i,j,rand.nextInt(9)+1)) {
-                    board[i][j]=rand.nextInt(9)+1;
-                    System.out.println(board[i][j]);
+                x=rand.nextInt(9)+1;
+                if(fits(i,j,x)){
+                    board[i][j]=x;
                 }
             }
         }
     }
 
-    boolean fits( int m, int n,int el){
+    boolean fits( int row, int col,int el){
         for(int i=0;i<9;i++){
-            if(board[m][i]==el) return false;
-            if(board[i][n]==el) return false;
+            if(board[row][i]==el) return false;
+            if(board[i][col]==el) return false;
         }
-
+        int startRow = row - row%3;
+        int startCol = col - col%3;
+        for(int i=startRow;i<startRow+3;i++){
+            for(int j=startCol;j<startCol+3;j++){
+                if(board[i][j]==el) return false;
+            }
+        }
         return true;
+    }
+
+    void print(){
+        for (int i=0;i<9;i++){
+            for (int j=0;j<9;j++) {
+                System.out.print(board[i][j]);
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
         SudokuBoard pawel = new SudokuBoard();
         pawel.fillBoard();
+        pawel.print();
     }
 }
