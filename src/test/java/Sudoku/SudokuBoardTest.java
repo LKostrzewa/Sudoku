f2c02665c67150e7480a6c87661157ee9a21eb93
Paweł Białek <pawelbialek@MacBook-Pro-Pawe.local>
2019-03-12 20:11:09 +0100

    Test improved

--- src/test/java/Sudoku/SudokuBoardTest.java
+++ src/test/java/Sudoku/SudokuBoardTest.java
@@ -4,10 +4,12 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import java.security.MessageDigest;
 
 import java.util.Arrays;
 
 class SudokuBoardTest {
+
 
     @Test
     public void CorrectBoardCreatingTest(){
@@ -23,7 +25,36 @@ class SudokuBoardTest {
         SudokuBoard sudoku2 = new SudokuBoard();
         sudoku2.fillBoard();
 
-        Assertions.assertTrue(!Arrays.equals(sudoku1.getBoard(),sudoku2.getBoard()));
+        int[][] pom = new int[9][9];
+        int[][] pom2 = new int[9][9];
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                pom[i][j] = i+j;
+                pom2[i][j] = i+j;
+            }
+        }
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                System.out.print(pom[i][j]);
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                System.out.print(pom2[i][j]);
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+
+
+
+
+        Assertions.assertTrue(!Arrays.deepEquals(sudoku1.getBoard(),sudoku2.getBoard()));
+        Assertions.assertTrue(Arrays.deepEquals(pom,pom2));
     }
     
 }
