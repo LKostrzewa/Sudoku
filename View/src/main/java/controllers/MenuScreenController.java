84fd070197f9c98babbc874937261550da1931c5
216804 Łukasz Kostrzewa <216804@edu.p.lodz.pl>
2019-05-20 00:20:54 +0200

    Implementacja poziomow trudnosci w interfejsie graficznym

--- View/src/main/java/controllers/MenuScreenController.java
+++ View/src/main/java/controllers/MenuScreenController.java
@@ -3,7 +3,7 @@ package controllers;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
-
+import sudoku.*;
 import java.awt.event.ActionEvent;
 
 public class MenuScreenController {
@@ -16,8 +16,15 @@ public class MenuScreenController {
 
     @FXML
     public void click() {
-        System.out.println("No elo");
+        SudokuBoard sudokuBoard = new SudokuBoard();
+        BacktrackingSudokuSolver solver = new BacktrackingSudokuSolver();
+        solver.solve(sudokuBoard);
+        Difficluty d1;
         System.out.println(choiceBox.getValue());
+        if (choiceBox.getValue().equals("Łatwy")) d1=Difficluty.EASY;
+        else if (choiceBox.getValue().equals("Normalny")) d1=Difficluty.MEDIUM;
+        else d1=Difficluty.HARD;
+        d1.clean(sudokuBoard);
     }
 
 }
