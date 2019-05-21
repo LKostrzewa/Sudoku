cd0f57a52b40a654eb8e5434f9ef54b4032279c8
216804 Łukasz Kostrzewa <216804@edu.p.lodz.pl>
2019-05-21 10:30:33 +0200

    Implementacja sprawdzania poprawności rozwiązanego sudoku w interfejsie graficznym

--- View/src/main/java/controllers/BoardController.java
+++ View/src/main/java/controllers/BoardController.java
@@ -9,6 +9,7 @@ import javafx.scene.layout.Pane;
 import jdk.internal.org.objectweb.asm.tree.IntInsnNode;
 import sudoku.SudokuBoard;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
 
 import java.awt.*;
 import java.io.IOException;
@@ -19,6 +20,7 @@ import java.util.ResourceBundle;
 public class BoardController {
 
     private MainController mainController;
+    private SudokuBoard sudokuBoard;
 
     @FXML
     private GridPane gridPane;
@@ -33,7 +35,8 @@ public class BoardController {
 
     public void prepare(MainController mainController) {
         setMainController(mainController);
-        SudokuBoard sudokuBoard = mainController.getSudokuBoard();
+        //SudokuBoard sudokuBoard = mainController.getSudokuBoard();
+        this.sudokuBoard = mainController.getSudokuBoard();
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
                 TextField textField = new TextField(Integer.toString(sudokuBoard.get(i, j)));
@@ -47,19 +50,7 @@ public class BoardController {
                 gridPane.add(textField, i, j);
             }
         }
-        //TextField txt = check();
-        //System.out.println(txt.getText());
     }
-    /* tu chcialem robic tak bo chciałem zrobic sprawdzanie kazdego pola na biezaco
-    public TextField check(){
-        List<Node> fields = gridPane.getChildren();
-        TextField kupka = new TextField();
-        for(Node i : fields){
-            test = (TextField) i;
-            if(((TextField) i).getText().equals("")) break;
-        }
-        return test;
-    }*/
 
     public void setMainController(MainController mainController) {
         this.mainController = mainController;
@@ -67,6 +58,24 @@ public class BoardController {
 
     @FXML
     public void onActionButton(){
-
+        List<Node> fields = gridPane.getChildren();
+        //SudokuBoard sudokuBoard = new SudokuBoard();
+        TextField test;
+        int row, col, val;
+        for(Node i : fields) {
+            test = (TextField) i;
+            row = gridPane.getRowIndex(i);
+            col = gridPane.getColumnIndex(i);
+            try{
+                val = Integer.parseInt(test.getText());
+            }
+            catch (NumberFormatException e){
+                val = 0 ; // tutaj nie do konca fancy ale zawsze cos :D
+                System.out.println("Bledny format danych");
+            }
+            this.sudokuBoard.set(row,col,val);
+        }
+        if(!sudokuBoard.checkBoard()) System.out.println("Podane sudoku nie jest prawidłowe");
+        else System.out.println("Wygrales !");
     }
 }
