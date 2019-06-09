ab6281be59fae45b5c512b56385f7fbcb33f8d5a
216723 Białek Paweł <216723@edu.p.lodz.pl>
2019-06-09 18:39:40 +0200

    Drobna refaktoryzacja + dodano jdbcSudokuBoardDao do fabryki

--- ModelProject/src/main/java/sudoku/JdbcSudokuBoardDao.java
+++ ModelProject/src/main/java/sudoku/JdbcSudokuBoardDao.java
@@ -7,9 +7,6 @@ import java.sql.*;
 import java.util.logging.Level;
 
 
-// Na razie tu jest bajzel i to nie zadziała
-
-
 public class JdbcSudokuBoardDao implements Dao<SudokuBoard>, AutoCloseable {
 
     String name;
@@ -18,24 +15,13 @@ public class JdbcSudokuBoardDao implements Dao<SudokuBoard>, AutoCloseable {
     ResultSet rs = null;
     Logger logger = LoggerFactory.getLogger(JdbcSudokuBoardDao.class);
 
-    //String url = "jdbc:derby:testdb;user=USER12";
-
     public JdbcSudokuBoardDao(final String name) {
         this.name = name;
 
         try {
             Class.forName("org.sqlite.JDBC");
-            //Connection con = DriverManager.getConnection("jdbc:derby:testdb;user=Romek");
-           // Connection con = DriverManager.getConnection("jdbc:derby:SudokuBoardsDB");
-            //st = con.createStatement();
         } catch (ClassNotFoundException e) {
             logger.error("Nie znaleziono klasy odpowiedzialnej za baze danych:\n" + e);
-        /*} catch (IllegalAccessException er) {
-            //Logger logger = LoggerFactory.getLogger(JdbcSudokuBoardDao.class);
-            logger.error("Brak dostepu:\n" + er);
-        } catch (InstantiationException err) {
-            //Logger logger = LoggerFactory.getLogger(JdbcSudokuBoardDao.class);
-            logger.error("Instcostam error:\n" + err);*/
         }
 
         try {
@@ -46,12 +32,9 @@ public class JdbcSudokuBoardDao implements Dao<SudokuBoard>, AutoCloseable {
         }
 
         try {
-            //con = DriverManager.getConnection("jdbc:derby:SudokuBoardsDB");
-            //st = con.createStatement();
             st.executeUpdate(
                     "CREATE TABLE IF NOT EXISTS BOARDS(" +
                             "name VARCHAR(30) PRIMARY KEY)");
-            //st.executeUpdate("CREATE TABLE IF NOT EXSISTS FIELDS(ID INT PRIMARY KEY, VALUE INT, BOARD_ID VARCHAR(30) FOREIGN KEY REFERENCES BOARDS(ID))");
             st.executeUpdate(
                     "CREATE TABLE IF NOT EXISTS FIELDS(" +
                             "row INT," +
@@ -60,7 +43,6 @@ public class JdbcSudokuBoardDao implements Dao<SudokuBoard>, AutoCloseable {
                             "id_board VARCHAR(30)," +
                             "FOREIGN KEY (id_board) REFERENCES BOARDS(name)," +
                             "PRIMARY KEY (id_board, row, col))");
-            //tu na razie tak bo nwm jak to id dziabnac i czy wgl jest potrzebne jak nie musi byc to wdg mnie nie jest
         } catch (SQLException e) {
             logger.error("Wyjatek sql :\n " + e);
         }
@@ -82,41 +64,14 @@ public class JdbcSudokuBoardDao implements Dao<SudokuBoard>, AutoCloseable {
                 }
             }
         } catch (SQLException e){
-
+            logger.error("Problem z odczytem z bazy danych:\n " + e);
         }
         return sudokuBoard;
-        /*try {
-
-            //System.setProperty("derby.system.home", "/home/janbodnar/.derby");
-
-            con = DriverManager.getConnection("jdbc:derby:SudokuBoardsDB");
-            st = con.createStatement();
-            rs = st.executeQuery("SELECT * FROM USER12.CARS");
-            while (rs.next()) {
-                System.out.print(rs.getInt(1));
-                System.out.print(" ");
-                System.out.print(rs.getString(2));
-                System.out.print(" ");
-                System.out.println(rs.getString(3));
-            }
-
-            DriverManager.getConnection("jdbc:derby:;shutdown=true");
-
-        } catch (SQLException ex) {
-            Logger logger = LoggerFactory.getLogger(JdbcSudokuBoardDao.class);
-            logger.error("No nie udalo sie odczytac :')");
-        }
-        return null;*/
     }
 
     @Override
     public void write(SudokuBoard obj) {
         try {
-            //System.setProperty("derby.system.home", "/home/janbodnar/.derby");
-            //con = DriverManager.getConnection("jdbc:sqlite:SudokuBoars.db");
-            //st = con.createStatement();
-            //st.executeUpdate("CREATE TABLE BOARDS(ID VARCHAR(30) PRIMARY KEY, NAME VARCHAR(30)");
-            //st.executeUpdate("CREATE TABLE FIELDS(ID INT PRIMARY KEY, VALUE INT, BOARD_ID VARCHAR(30) FOREIGN KEY REFERENCES BOARDS(ID))");
             String insertBrd = "INSERT INTO BOARDS VALUES(\'" + name + "\')";
             st.execute(insertBrd);
             for(int i = 0; i < SudokuBoard.BOARD_SIZE; i++){
@@ -125,17 +80,8 @@ public class JdbcSudokuBoardDao implements Dao<SudokuBoard>, AutoCloseable {
                     st.execute(insertField);
                 }
             }
-        } catch (SQLException ex) {
-
-            // Ten catch to nw
-            Logger logger = LoggerFactory.getLogger(JdbcSudokuBoardDao.class);
-            if (((ex.getErrorCode() == 50000)
-                    && ("XJ015".equals(ex.getSQLState())))) {
-
-                logger.debug(Level.INFO + "Derby shut down normally" + ex);
-            } else {
-                logger.debug(Level.SEVERE + ex.getMessage() + ex);
-            }
+        } catch (SQLException e) {
+            logger.error("Problem z zapisem do bazy danych:\n " + e);
         }
     }
 
@@ -146,7 +92,10 @@ public class JdbcSudokuBoardDao implements Dao<SudokuBoard>, AutoCloseable {
             insertBrd = "DELETE FROM FIELDS WHERE id_board=\'" + name + "\';";
             st.execute(insertBrd);
         } catch (SQLException e) {
-            e.printStackTrace();
+            logger.error("Problem z czysczeniem do bazy danych:\n " + e);
+            //Throwable throwable = new FileExeption("Problem z czysczeniem do bazy danych:\n ", e);
+            //throwable.initCause(e);
+            //throw throwable;
         }
     }
 
@@ -163,7 +112,6 @@ public class JdbcSudokuBoardDao implements Dao<SudokuBoard>, AutoCloseable {
                 rs.close();
             }
         } catch (SQLException e) {
-            Logger logger = LoggerFactory.getLogger(JdbcSudokuBoardDao.class);
             logger.error("Zamknięcie bazy danych nie powiodło się: " + e);
         }
     }
--- ModelProject/src/main/java/sudoku/SudokuBoardDaoFactory.java
+++ ModelProject/src/main/java/sudoku/SudokuBoardDaoFactory.java
@@ -2,6 +2,7 @@ package sudoku;
 
 public class SudokuBoardDaoFactory {
 
+    // No nw czy to tak ma byc
     public static FileSudokuBoardDao
     getSudokuBoardDaoFactory(final String fileName) {
         /*try (FileSudokuBoardDao file =
@@ -12,8 +13,13 @@ public class SudokuBoardDaoFactory {
             System.out.println("Nie znaleziono pliku123");
             return null;
         }*/
-        // Before:
-        return new FileSudokuBoardDao(fileName);
 
+        // Before:
+        //return new FileSudokuBoardDao(fileName);
+        return new FileSudokuBoardDao(fileName);
+    }
+
+    public static JdbcSudokuBoardDao getJDBCdao(final String fileName) {
+        return new JdbcSudokuBoardDao(fileName);
     }
 }
